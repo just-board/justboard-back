566d0905e764007b854c8f41ddc7f8e36497f98e
CINY4612 <ciy5364612@gmail.com>
2022-10-24 23:19:24 +0900

    Project Setup

---
+++ src/main/java/com/board/justboard/JustboardApplication.java
@@ -0,0 +1,13 @@
+package com.board.justboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustboardApplication.class, args);
+    }
+
+}
